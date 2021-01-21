aaddf64d55b587238e2aa12a2e2f5a1e953e0250
rafaelpapastamatiou <rafaelpapas1412@gmail.com>
2021-01-20 22:37:46 -0300

    cors added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 
+import cors from 'cors';
+
 import express, { Request, Response, NextFunction } from 'express';
 
 import 'express-async-errors';
@@ -14,6 +16,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
