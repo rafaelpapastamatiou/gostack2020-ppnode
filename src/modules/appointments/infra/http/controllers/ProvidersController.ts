7590872d1cb027f3be7824750c66deeeab9e7646
rafaelpapastamatiou <rafaelpapas1412@gmail.com>
2021-02-06 16:34:41 -0300

    classToClass providers list

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
 
+import { classToClass } from 'class-transformer';
+
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
 export default class ProvidersController {
@@ -14,6 +16,6 @@ export default class ProvidersController {
       user_id,
     });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
